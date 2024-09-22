ccc79f61401186d6b1b56708d8d93f16f5257cfa
world-dv <goodforx0147@naver.com>
2024-09-22 16:54:01 +0900

    feat: client thread

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.receive.ReceiveThread;
+import org.example.send.SendThread;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 8888);
+        System.out.println("Connect Server");
+
+        ReceiveThread receiveThread = new ReceiveThread(socket);
+        receiveThread.start();
+
+        SendThread sendThread = new SendThread(socket);
+        sendThread.start();
     }
 }
